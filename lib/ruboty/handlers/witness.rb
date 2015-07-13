cd01068e252fd5961f33884effb17850f609272c
HIDEKUNI Kajita <hide.nba@gmail.com>
2015-07-13 17:26:22 +0900

    写真取れるようにした

--- lib/ruboty/handlers/witness.rb
+++ lib/ruboty/handlers/witness.rb
@@ -10,7 +10,13 @@ module Ruboty
       def say(message)
         aquestalk_path = ENV['AQUES_TALK_PATH']
         `#{aquestalk_path}/AquesTalkPi -b '#{message[:talk_text]}' | aplay`
-        message.reply(message[:talk_text])
+
+        image = Tempfile.new(%w(witness .jpg))
+        `raspistill -w 1024 -h 768 -q 50 -o #{image.path}`
+
+        message.reply(image.path)
+
+        image.close
      end
     end
   end
