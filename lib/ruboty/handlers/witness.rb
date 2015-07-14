85bbdfbf594ac25f563fba487dd5c824efff8f4c
HIDEKUNI Kajita <hide.nba@gmail.com>
2015-07-14 09:30:14 +0900

    refacter

--- lib/ruboty/handlers/witness.rb
+++ lib/ruboty/handlers/witness.rb
@@ -10,24 +10,31 @@ module Ruboty
       )
 
       def say(message)
+        talk(message[:talk_text])
+        message.reply(still)
+      end
+
+      private
+
+      def talk(text)
         aquestalk_path = ENV['AQUES_TALK_PATH']
-        `#{aquestalk_path}/AquesTalkPi -b '#{message[:talk_text]}' | aplay`
+        `#{aquestalk_path}/AquesTalkPi -b '#{text}' | aplay`
+      end
 
-        image = Tempfile.new(%w(witness .jpg))
-        `raspistill -w 1024 -h 768 -q 50 -o #{image.path}`
+      def still
+        Tempfile.open(%w(witness .jpg)) do |image|
+          `raspistill -w 1024 -h 768 -q 50 -o #{image.path}`
+          upload_s3(image.path)
+        end
+      end
 
-        message.reply(image.path)
-
+      def upload_s3(local_path)
         file_name = "#{SecureRandom.hex}.jpg"
-
         s3 = Aws::S3::Resource.new(region: 'ap-northeast-1')
         obj = s3.bucket('raspberrypi-witness').object(file_name)
-        obj.upload_file(image.path)
-
-        message.reply(obj.public_url)
-
-        image.close
-     end
+        obj.upload_file(local_path)
+        obj.public_url
+      end
     end
   end
 end
